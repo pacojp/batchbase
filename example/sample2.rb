36dbfaa4b6cb848ad62386710344b9cfd4edf409
Kazuaki Harada <paco@Kazuakis-MacBook-Pro.local>
2012-09-19 12:53:35 +0900

    harada up

--- example/sample2.rb
+++ example/sample2.rb
@@ -10,7 +10,17 @@ class Batch < Batchbase::LogFormatter
     execute do
       p environment
       info 'info message'
-      raise 'error'
+
+      db_yml_path = File::dirname(environment[:pg_path]) + '/../config/database.yml'
+      # 第3引数は複数DB接続が無いならば指定不要
+      db_config = Batchbase::Mysql2Wrapper.config_from_yml(db_yml_path,environment[:env],'some_database')
+
+      client = Batchbase::Mysql2Wrapper.new(db_config)
+      client.query "SELECT * FROM hoges"
+      client.transaction do
+        client.query 'SELECT * FROM hoges'
+        raise 'error' # call ROLLBACK
+      end
     end
   end
 end
--- lib/batchbase/mysql2_wrapper.rb
+++ lib/batchbase/mysql2_wrapper.rb
@@ -3,13 +3,54 @@
 class Batchbase::Mysql2Wrapper
   attr_accessor :client
 
+  QUERY_BASE_COLOR = 35
+  QUERY_SPECIAL_COLOR = 31
+
   def initialize(config)
     Batchbase::LogFormatter.info "mysql2 client created with #{config.inspect}"
     self.client = Mysql2::Client.new(config)
+    # サーバが古いので一応問題あるけど以下の方向で
+    # http://kennyqi.com/archives/61.html
+    self.class.query(self.client,"set names utf8")
   end
 
-  def query(str)
-    Batchbase::LogFormatter.info "[QUERY] " + str
-    self.client.query(str)
+  def self.query(client,str,color=QUERY_BASE_COLOR)
+    Batchbase::LogFormatter.info "[QUERY] \e[#{color}m#{str}\e[0m"
+    client.query(str)
+  end
+
+  def query(str,color=QUERY_BASE_COLOR)
+    self.class.query(self.client,str,color)
+  end
+
+  def transaction(&proc)
+    raise ArgumentError, "No block was given" unless block_given?
+    #query "SET AUTOCOMMIT=0;",QUERY_SPECIAL_COLOR
+    query "BEGIN",QUERY_SPECIAL_COLOR
+    begin
+      yield
+      query "COMMIT",QUERY_SPECIAL_COLOR
+    rescue => e
+      query "ROLLBACK",QUERY_SPECIAL_COLOR
+      raise e
+    end
+  end
+
+  def self.make_config_key_symbol(config)
+    new_config = {}
+    config.each do |key,value|
+      new_config[key.to_sym] = value
+    end
+    config = new_config
+  end
+
+  def self.config_from_yml(yml_path,environment,db_name=nil)
+    db_config = YAML.load_file(yml_path)[environment]
+    if db_name
+      db_config = Batchbase::Mysql2Wrapper.make_config_key_symbol(db_config[db_name])
+    else
+      db_config = Batchbase::Mysql2Wrapper.make_config_key_symbol(db_config)
+    end
+    db_config
   end
 end
