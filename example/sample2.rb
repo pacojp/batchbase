73d43f27f89ecbf0143fda212dc661ff2ca07e77
Kazuaki Harada <paco@pacos-MacBook-Pro.local>
2012-10-26 11:21:15 +0900

    サンプルを更新

--- example/sample2.rb
+++ example/sample2.rb
@@ -8,13 +8,14 @@ class Batch
 
   def proceed
     opts = self.option_parser
+    options_arg = {:favorite_number=>777} #初期値を設定
     opts.on("-f", "--favorite_number=value",
            Integer,"favo"
             ) do |v|
       env[:favorite_number] = v
     end
 
-    execute do
+    execute(options_arg) do
       logger.info env.inspect
       if env[:favorite_number]
         logger.info env[:favorite_number].to_s
